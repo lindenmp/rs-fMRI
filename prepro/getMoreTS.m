b8eab3145d5efea807c0bcbd9302a989264b02d3
lindenmp <lindenparkes@gmail.com>
2017-05-16 14:07:41 +1000

    no message

--- prepro/getMoreTS.m
+++ prepro/getMoreTS.m
@@ -1,72 +1,48 @@
-function [] = getMoreTS(WhichProject,subject)
-    % ------------------------------------------------------------------------------
-    % Set project settings and parameters
-    % Use WhichProject if you're juggling multiple datasets
-    % ------------------------------------------------------------------------------
-    switch WhichProject
-        case 'OCDPG'
-            % Where the subjects' directories are
-            datadir = '/gpfs/M2Home/projects/Monash076/Linden/OCDPG/data/';
-             
-            rawdir = [datadir,subject,'/rfMRI/'];
+% ------------------------------------------------------------------------------
+% Set project variables
+% ------------------------------------------------------------------------------
+projdir = '/gpfs/M2Home/projects/Monash076/Linden/OCDPG/';
+sublist = '/gpfs/M2Home/projects/Monash076/Linden/Sublists/OCDPG.txt';
+datadir = [projdir,'data/'];
 
-            % where the processed epi 4d files will be output to from prepro_base
-            preprodir = [rawdir,'prepro/'];
-    end
+fileID = fopen(sublist);
+ParticipantIDs = textscan(fileID,'%s');
+ParticipantIDs = ParticipantIDs{1};
 
-    
-    % All pipelines
-    noiseOptions = {'6P','6P+2P','6P+2P+GSR','24P','24P+8P','24P+8P+4GSR','24P+8P+SpikeReg','24P+8P+4GSR+SpikeReg','12P+aCC','24P+aCC','12P+aCC50','24P+aCC50','24P+aCC+4GSR','24P+aCC50+4GSR','24P+aCC+SpikeReg','24P+aCC+4GSR+SpikeReg','ICA-AROMA+2P','ICA-AROMA+2P+SpikeReg','ICA-AROMA+GSR','ICA-AROMA+2P+GSR','ICA-AROMA+8P','ICA-AROMA+4GSR','ICA-AROMA+8P+4GSR'};
+% All pipelines
+noiseOptions = {'6P','6P+2P','6P+2P+GSR','24P','24P+8P','24P+8P+4GSR','24P+8P+SpikeReg','24P+8P+4GSR+SpikeReg','12P+aCC','24P+aCC','12P+aCC50','24P+aCC50','24P+aCC+4GSR','24P+aCC50+4GSR','24P+aCC+SpikeReg','24P+aCC+4GSR+SpikeReg','ICA-AROMA+2P','ICA-AROMA+2P+SpikeReg','ICA-AROMA+GSR','ICA-AROMA+2P+GSR','ICA-AROMA+8P','ICA-AROMA+4GSR','ICA-AROMA+8P+4GSR'};
 
-    % Loop over noise correction options
-    % for i = 1:length(noiseOptions)
-    for i = 1:1
-        removeNoise = noiseOptions{i};
+% Loop over noise correction options
+for i = 1:1
+    subject = ParticipantIDs{i};
+    preprodir = [datadir,subject,'/rfMRI/prepro/'];
+
+    for j = 1:1
+        removeNoise = noiseOptions{j};
 
         cd([preprodir,removeNoise])
-
         load('cfg.mat')
+        cd(cfg.outdir)
+        
+        % Parcellation file for time series extraction
+        cfg.new_parcFiles = {'/gpfs/M2Home/projects/Monash076/Linden/ROIs/TriStri/Striatum.nii'};
+        cfg.parcFiles{end+1} = cfg.new_parcFiles{1}
 
-        % ------------------------------------------------------------------------------
-        % extract time series
-        runTS = 1;
-        % ------------------------------------------------------------------------------
-        if runTS == 1
-            cd(cfg.outdir)
-            
-            % Parcellation file for time series extraction
-            cfg.new_parcFiles = {'/gpfs/M2Home/projects/Monash076/Linden/ROIs/TriStri/Striatum.nii'};
+        cfg.new_parcWeightGM = {'no'};
+        cfg.parcWeightGM{end+1} = cfg.new_parcWeightGM{1}
 
-            cfg.new_parcWeightGM = {'no'};
-
-            % Set input image for time series extraction
-            switch cfg.smoothing
-                case {'before','none'}
-                    % If smoothing was done before noise correction:
-                    cfg.ExtractIn = 'epi_prepro.nii';
-                case 'after'
-                    % If it was done after:
-                    cfg.ExtractIn = 'sepi_prepro.nii';
-            end
-
-            % Initialise roi time series variable
-            % cfg.roiTS = [];
-
-            % Loop over parcellation files
-            for i = 1:length(cfg.new_parcFiles)
-                % Set parcellation file
-                cfg.parcFile = cfg.new_parcFiles{i};
-                % set GM weight
-                cfg.weightGM = cfg.new_parcWeightGM{i};
-                % extract time series 
-                cfg.roiTS{end+1} = prepro_extractTS_FSL(cfg);
-            end
-
-            cfg = rmfield(cfg,{'parcFile','weightGM'});
+        % Loop over parcellation files
+        for k = 1:length(cfg.new_parcFiles)
+            % Set parcellation file
+            cfg.parcFile = cfg.new_parcFiles{k};
+            % set GM weight
+            cfg.weightGM = cfg.new_parcWeightGM{k};
+            % extract time series 
+            cfg.roiTS{end+1} = prepro_extractTS_FSL(cfg);
         end
 
-        % Save data
-        save('cfg.mat','cfg')
+        cfg = rmfield(cfg,{'parcFile','weightGM'});
     end
-
+    % Save data
+    save('cfg.mat','cfg')
 end
