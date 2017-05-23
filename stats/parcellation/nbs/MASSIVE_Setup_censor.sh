f01962999aee95edaa5f2573a9037beb18a2e35f
lindenmp <lindenparkes@gmail.com>
2017-05-23 17:16:30 +1000

    no message

--- stats/parcellation/nbs/MASSIVE_Setup_censor.sh
+++ stats/parcellation/nbs/MASSIVE_Setup_censor.sh
@@ -4,7 +4,7 @@ for WhichCensor in spikeReg scrub; do
 	for WhichSplit in Diagnostic Motion; do
 		for WhichParc in Gordon; do
 			for WhichNoise in 6P 6P+2P 6P+2P+GSR 24P 24P+8P 24P+8P+4GSR 24P+aCC 24P+aCC+4GSR 24P+aCC50 24P+aCC50+4GSR 12P+aCC 12P+aCC50 sICA-AROMA+2P sICA-AROMA+2P+GSR sICA-AROMA+8P sICA-AROMA+8P+4GSR; do
-				JobName=${WhichProject}_${WhichSplit}_${WhichParc}_${WhichNoise}
+				JobName=${WhichProject}_${WhichSplit}_${WhichParc}_${WhichNoise}_${WhichCensor}
 				echo $JobName
 				sbatch --output=${JobName}.out MASSIVE_Job.sh ${WhichProject} ${WhichSplit} ${WhichParc} ${WhichNoise} 0.05 NBS_tDOF_${WhichCensor} ${WhichCensor}
 				sleep 1
