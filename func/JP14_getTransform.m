c1b499bdeeedbd5df1a0123f293feb8016da01e8
lindenmp <lindenparkes@gmail.com>
2017-08-25 15:57:45 +1000

    cleaned up and commented

--- func/JP14_getTransform.m
+++ func/JP14_getTransform.m
@@ -5,17 +5,37 @@
 % Adapted by Linden Parkes for use in Parkes et al., 2017. bioRxiv
 % This produces interpolated fMRI time series using censored data
 % ------------------------------------------------------------------------------
-function [H,f,s,c,tau,w] = JP14_getTransform(t,h,TH,Tr,ofac,hifac)
+function [H,f,s,c,tau,w] = JP14_getTransform(t,data,TH,Tr,ofac,hifac)
 
-	%Input t is a column vector listing the time points for which observations
-	%are present.  Input h is a matrix with observations in columns and the
-	%number of rows equals the number the time points.  For our purposes number
-	%of voxels = number of columns.  Ofac = oversampling frequency (generally
-	%>=4), hifac = highest frequency allowed.  hifac = 1 means 1*nyquist limit
-	%is highest frequency sampled.  
-	%Lasted edited:  Anish Mitra, October 25 2012
+	% Inputs
+	% 
+	% t 	- column vector listing the *censored* time points in seconds
+	%		e.g., t = ([1:N]') * TR;
+	% 		2
+	% 		4
+	% 		6
+	% 		12
+	% 		14
+	% 		16
+	% 		Note the missing time points between 6 and 12
+	% 
+	% data 	- N (num time points) * numVoxels matrix of *censored* fMRI time series data
+	% 
+	% TH 	- column vector listing the *uncensored* time points in seconds
+	% 
+	% Tr 	- TR of fMRI sequences in seconds. e.g., TR = 2
+	% 
+	% ofac 	- oversampling frequency (generally >=4 )
+	% hifac - highest frequency allowed
 
-	N = size(h,1); %Number of time points
+	% Outputs
+	% H 	- the output time series reconstructed from the basis functions.
+	% 		The censored sections of this time series need to be substituted back into the
+	% 		original time series. That is, this is NOT the original time series + interpolated
+	% 		volumes. It is simply the reconstructed time series.
+
+
+	N = size(data,1); %Number of time points
 	T = max(t) - min(t); %Total time span
 
 	%calculate sampling frequencies
@@ -29,24 +49,17 @@ function [H,f,s,c,tau,w] = JP14_getTransform(t,h,TH,Tr,ofac,hifac)
 	cterm = cos(w*t.' - repmat(w.*tau,1,length(t)));
 	sterm = sin(w*t.' - repmat(w.*tau,1,length(t)));
 
-	num_tseries = size(h,2); %Number of time series
+	num_tseries = size(data,2); %Number of time series
 
-	% D = bsxfun(@minus,h,mean(h));  %This line involved in normalization
-	D = h;
+	D = data;
 	D = reshape(D,1,N,num_tseries);
 
-
-	%% C_final = (sum(Cmult,2).^2)./sum(Cterm.^2,2);
 	% This calculation is done by separately for the numerator, denominator,
 	% and the division
 
 	Cmult = bsxfun(@times, cterm,D);
-	%rewrite the above line with bsxfun to optimize further?
-	%bsxfun(@power,sum(Cmult,2),2) = sum(Cmult.^2,2) = numerator
-	% numerator = bsxfun(@power,sum(Cmult,2),2);
 	numerator = sum(Cmult,2); %Modify the numerator to get the cw term
 
-	%sum(bsxfun(@power,Cterm,2),2) = sum(Cterm.^2,2) = denominator
 	denominator = sum(bsxfun(@power,cterm,2),2); %use Cterm in place of cterm to make it exactly the denominator in the original expression
 	C_final_new = bsxfun(@rdivide,numerator,denominator);
 	c = C_final_new;
@@ -54,24 +67,15 @@ function [H,f,s,c,tau,w] = JP14_getTransform(t,h,TH,Tr,ofac,hifac)
 
 	%% Repeat the above for Sine term
 	Smult = bsxfun(@times, sterm,D);
-	% S_final = (sum(Smult,2).^2)./sum(Sterm.^2,2);
-	% numerator = bsxfun(@power,sum(Smult,2),2);
 	numerator = sum(Smult,2); %Modify the numerator to get the sw term
 	denominator = sum(bsxfun(@power,sterm,2),2);
 	S_final_new = bsxfun(@rdivide,numerator,denominator);
 	s = S_final_new;
 	clear numerator denominator sterm Smult S_final_new
 
-	% %% Power = C_final + S_final; 
-	% Power = C_final_new + S_final_new;
-	% Power_reshaped = reshape(Power,size(Power,1),num_tseries);
-	% Power_final = bsxfun(@rdivide,Power_reshaped,2*var(h)); %Normalize the power
-	% clearvars -except Power_final f
-
-
 	% The inverse function to re-construct the original time series
 	Time = TH';
-	T_rep = repmat(Time,[size(f,1),1,size(h,2)]);
+	T_rep = repmat(Time,[size(f,1),1,size(data,2)]);
 	% T_rep = bsxfun(@minus,T_rep,tau);
 	% s(200) = s(199);
 	w = 2*pi*f;
@@ -83,11 +87,11 @@ function [H,f,s,c,tau,w] = JP14_getTransform(t,h,TH,Tr,ofac,hifac)
 	S = sum(sw_p);
 	C = sum(cw_p);
 	H = C + S;
-	H = reshape(H,size(Time,2),size(h,2));
+	H = reshape(H,size(Time,2),size(data,2));
 
 	%Normalize the reconstructed spectrum, needed when ofac > 1
 	Std_H = std(H);
-	Std_h = std(h);
+	Std_h = std(data);
 	norm_fac = Std_H./Std_h;
 	H = bsxfun(@rdivide,H,norm_fac);
 end
