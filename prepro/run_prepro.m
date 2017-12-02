a15da5dbb398cb3559bf448599f0c31c166d4fbc
lindenmp <lindenparkes@gmail.com>
2017-12-02 15:14:54 +1100

    OCDPG_DCM project added

--- prepro/run_prepro.m
+++ prepro/run_prepro.m
@@ -710,6 +710,49 @@ function [] = run_prepro(WhichProject,WhichSessScan,subject,smoothing,discard,sl
             cfg.LowPass = 0.08;
             % Hi-pass cut-off for bandpass filter in Hz (e.g., .008)
             cfg.HighPass = 0.008;
+        case 'OCDPG_DCM'
+            % Where the subjects' directories are
+            cfg.datadir = [cfg.parentdir_scratch,'ResProjects/rfMRI_DCM/OCDPG/data/'];
+
+            switch cfg.WhichSessScan
+                case 'Sess1_Scan1'
+                    % where the unprocessed EPI 4d file is
+                    cfg.rawdir = [cfg.datadir,cfg.subject,'/func/'];
+                    % Directory where the t1 is
+                    cfg.t1dir = [cfg.datadir,cfg.subject,'/anat/'];
+            end
+
+            % where the processed epi 4d files will be output to from prepro_base
+            cfg.preprodir = [cfg.rawdir,'prepro/'];
+
+            % file name of EPI 4d file
+            cfg.EPI = [cfg.subject,'_task-rest_bold.nii'];
+            % name of t1 file.
+            cfg.t1name = [cfg.subject,'_T1w.nii'];
+
+            % the path and filename of the template in MNI space to which everything
+            % will be normalized
+            cfg.mni_template = [cfg.spmdir,'templates/T1.nii'];
+
+            % preprocessing settings
+            % length of time series (no. vols)
+            cfg.N = 189;
+            % Repetition time of acquistion in secs
+            cfg.TR = 2.5;
+            % Number of slices in EPI volumes.
+            cfg.numSlices = 44;
+            % Vector defining acquisition order of EPI slices (necessary for slice-timing correction.
+            % See help of slicetime_epis.m for guidance on how to define)
+            cfg.order = [1:2:cfg.numSlices,2:2:cfg.numSlices]; % interleaved
+            % Reference slice for slice timing acquisition. See SlicetimeEPI.m for guidance on how to define.
+            cfg.refSlice = cfg.numSlices-1; % use for interleaved order
+
+            % Scalar value indicating spatial smoothing kernal size in mm.
+            cfg.kernel = 6;
+            % Low-pass cut-off for bandpass filter in Hz (e.g., .08)
+            cfg.LowPass = 0.08;
+            % Hi-pass cut-off for bandpass filter in Hz (e.g., .008)
+            cfg.HighPass = 0.008;
     end
 
     % ------------------------------------------------------------------------------
