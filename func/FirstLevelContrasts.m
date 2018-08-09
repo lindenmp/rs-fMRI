6c33ee516f0a3c4e5612c91fa25b0d666a9c16f5
lindenmp <lindenparkes@gmail.com>
2018-08-09 20:47:09 +1000

    bilateral contrasts

--- func/FirstLevelContrasts.m
+++ func/FirstLevelContrasts.m
@@ -33,6 +33,18 @@ switch WhichSeed
 		matlabbatch{1}.spm.stats.con.consess{3}.tcon.convec = [0 0 1];
 		matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
 		matlabbatch{1}.spm.stats.con.delete = 0;
+	case 'TriStri_B'
+		matlabbatch{1}.spm.stats.con.spmmat = {matfile};
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'Sensorimotor';
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.convec = [1 0 0 1 0 0];
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'Associative';
+		matlabbatch{1}.spm.stats.con.consess{2}.tcon.convec = [0 1 0 0 1 0];
+		matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'Ventral';
+		matlabbatch{1}.spm.stats.con.consess{3}.tcon.convec = [0 0 1 0 0 1];
+		matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.delete = 0;
 	case 'DiMartino'
 		matlabbatch{1}.spm.stats.con.spmmat = {matfile};
 		matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'VSi';
@@ -54,6 +66,33 @@ switch WhichSeed
 		matlabbatch{1}.spm.stats.con.consess{6}.tcon.convec = [0 0 0 0 0 1];
 		matlabbatch{1}.spm.stats.con.consess{6}.tcon.sessrep = 'none';
 		matlabbatch{1}.spm.stats.con.delete = 0;
+	case 'DiMartino_B'
+		matlabbatch{1}.spm.stats.con.spmmat = {matfile};
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'VSi';
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.convec = [1 0 0 0 0 0 1 0 0 0 0 0];
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'VSs';
+		matlabbatch{1}.spm.stats.con.consess{2}.tcon.convec = [0 1 0 0 0 0 0 1 0 0 0 0];
+		matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'DC';
+		matlabbatch{1}.spm.stats.con.consess{3}.tcon.convec = [0 0 1 0 0 0 0 0 1 0 0 0];
+		matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'DCP';
+		matlabbatch{1}.spm.stats.con.consess{4}.tcon.convec = [0 0 0 1 0 0 0 0 0 1 0 0];
+		matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'DRP';
+		matlabbatch{1}.spm.stats.con.consess{5}.tcon.convec = [0 0 0 0 1 0 0 0 0 0 1 0];
+		matlabbatch{1}.spm.stats.con.consess{5}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.consess{6}.tcon.name = 'VRP';
+		matlabbatch{1}.spm.stats.con.consess{6}.tcon.convec = [0 0 0 0 0 1 0 0 0 0 0 1];
+		matlabbatch{1}.spm.stats.con.consess{6}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.delete = 0;
+	case 'GSR'
+		matlabbatch{1}.spm.stats.con.spmmat = {matfile};
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'GSR';
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.convec = [1];
+		matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+		matlabbatch{1}.spm.stats.con.delete = 0;		
 end
 
 spm_jobman('run',matlabbatch);
